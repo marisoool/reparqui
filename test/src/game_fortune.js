2b415dd401726879a52d14c590d5d9d707dd0a5b
marisoool <desarrolloweb9@gmail.com>
2023-09-08 18:26:13 -0600

    Strikes implementation

--- test/src/game_fortune.js
+++ test/src/game_fortune.js
@@ -1,6 +1,6 @@
 //	QUESTION FUNCTIONS
 
-
+let svgImg = '<svg fill="#ff0000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 328.157 328.157" xml:space="preserve" stroke="#ff0000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g id="XMLID_169_"> <path id="XMLID_197_" d="M267.169,219.647c19.417-19.417,19.417-51.01,0-70.427l-32.103-32.103l32.103-32.103 c19.417-19.417,19.416-51.011,0-70.428C257.763,5.18,245.257,0,231.956,0c-13.302,0-25.808,5.18-35.214,14.586l-32.103,32.103 l-32.103-32.103C123.129,5.18,110.624,0,97.322,0S71.514,5.18,62.108,14.586c-19.417,19.417-19.417,51.011,0,70.428l32.103,32.103 L62.108,149.22c-19.417,19.417-19.417,51.01,0,70.428c9.406,9.406,21.912,14.586,35.214,14.586s25.808-5.18,35.214-14.586 l32.103-32.103l32.103,32.103c9.406,9.406,21.912,14.586,35.214,14.586C245.257,234.233,257.763,229.053,267.169,219.647z"></path> <path id="XMLID_221_" d="M67.681,299.317c-1.52-1.306-3.374-2.4-5.56-3.28c-2.188-0.88-4.64-1.692-7.36-2.44 c-1.974-0.533-3.708-1.013-5.2-1.44c-1.494-0.426-2.734-0.893-3.72-1.4c-0.987-0.506-1.733-1.08-2.24-1.72 c-0.508-0.64-0.76-1.44-0.76-2.4c0-3.146,2.346-4.72,7.04-4.72c1.707,0,3.413,0.24,5.12,0.72c1.706,0.48,3.28,1.04,4.72,1.68 c1.44,0.64,2.64,1.254,3.6,1.84c0.96,0.587,1.52,0.987,1.68,1.2l5.76-10.88c-1.28-0.746-2.68-1.493-4.2-2.24 c-1.52-0.747-3.16-1.413-4.92-2c-1.76-0.586-3.6-1.066-5.52-1.44c-1.92-0.372-3.868-0.56-5.84-0.56c-2.827,0-5.507,0.4-8.04,1.2 c-2.534,0.8-4.76,1.988-6.68,3.56c-1.92,1.574-3.44,3.507-4.56,5.8c-1.12,2.294-1.68,4.934-1.68,7.92c0,2.134,0.307,4,0.92,5.6 c0.613,1.6,1.546,3.014,2.8,4.24c1.252,1.228,2.84,2.308,4.76,3.24c1.92,0.934,4.187,1.8,6.8,2.6c1.972,0.587,3.8,1.12,5.48,1.6 c1.68,0.48,3.132,1,4.36,1.56c1.226,0.56,2.187,1.188,2.88,1.88c0.693,0.694,1.04,1.574,1.04,2.64c0,2.828-2.32,4.24-6.96,4.24 c-2.188,0-4.348-0.292-6.48-0.88c-2.134-0.586-4.067-1.252-5.8-2c-1.734-0.746-3.188-1.492-4.36-2.24 c-1.173-0.747-1.867-1.252-2.08-1.52l-5.76,11.44c1.652,1.12,3.453,2.107,5.4,2.96c1.946,0.854,3.96,1.587,6.04,2.2 c2.08,0.614,4.212,1.08,6.4,1.4c2.187,0.32,4.32,0.48,6.4,0.48c2.72,0,5.36-0.308,7.92-0.92c2.56-0.613,4.826-1.6,6.8-2.96 c1.973-1.36,3.56-3.146,4.76-5.36c1.2-2.212,1.8-4.92,1.8-8.12c0-2.612-0.414-4.852-1.24-6.72 C70.373,302.211,69.201,300.625,67.681,299.317z"></path> <polygon id="XMLID_222_" points="75.24,282.237 92.6,282.237 92.6,327.517 105.72,327.517 105.72,282.237 123,282.237 123,270.717 75.24,270.717 "></polygon> <path id="XMLID_223_" d="M168.159,303.477c1.146-1.146,2.12-2.44,2.92-3.88c0.8-1.44,1.413-2.973,1.84-4.6 c0.426-1.626,0.64-3.32,0.64-5.08c0-2.346-0.468-4.666-1.4-6.96c-0.934-2.292-2.228-4.346-3.88-6.16 c-1.654-1.813-3.614-3.28-5.88-4.4c-2.268-1.12-4.734-1.68-7.4-1.68h-25.6v56.8h13.12v-18.32h8.56l10.88,18.32h14.8l-12.48-21.12 C165.719,305.597,167.012,304.625,168.159,303.477z M158.519,295.557c-1.12,1.467-2.454,2.2-4,2.2h-12v-15.52h11.6 c1.492,0,2.88,0.693,4.16,2.08c1.28,1.388,1.92,3.254,1.92,5.6C160.199,292.211,159.639,294.091,158.519,295.557z"></path> <rect id="XMLID_226_" x="182.999" y="270.717" width="13.12" height="56.8"></rect> <polygon id="XMLID_227_" points="255.638,270.717 240.917,270.717 220.118,295.197 220.118,270.717 206.998,270.717 206.998,327.517 220.118,327.517 220.118,310.797 226.038,304.637 242.118,327.517 257.078,327.517 234.357,296.077 "></polygon> <polygon id="XMLID_228_" points="274.437,315.997 274.437,303.917 296.837,303.917 296.837,293.277 274.437,293.277 274.437,282.237 300.517,282.237 300.517,270.717 261.317,270.717 261.317,327.517 301.237,327.517 301.237,315.997 "></polygon> </g> </g></svg>';
 
 //crea elemento
 function gameFortune(game,response){
@@ -42,7 +42,27 @@ function gameFortune(game,response){
     let labelKey = createElementJS('label','lblKey');
     labelKey.innerText='Adivinar frase';
     let inputGuess = createElementJS('input','guessPhraseTxt',{'type':'text','style':'display:none;'});
-    let chances = createElementJS('span','chancesRecord');
+
+
+    let chances = createElementJS('span','strikes');
+	let s1 = createElementJS('span','strike1');
+	let s2 = createElementJS('span','strike2');
+	let s3 = createElementJS('span','strike3');
+
+	s1.innerHTML = svgImg.replace('Capa_1','strike1');
+	s2.innerHTML = svgImg.replace('Capa_1','strike2');
+	s3.innerHTML = svgImg.replace('Capa_1','strike3');
+
+	s1.style.display = 'none';
+	s2.style.display = 'none';
+	s3.style.display = 'none';
+
+	chances.appendChild(s1);
+	chances.appendChild(s2);
+	chances.appendChild(s3);
+
+
+	globals['strikes']=0;
     optionKey.appendChild(optionKeySel);
     optionKey.appendChild(labelKey);
     optionKey.appendChild(chances);
@@ -51,10 +71,6 @@ function gameFortune(game,response){
     game.appendChild(optionKey);
     
     
-    
-    globals['f_chances']=0;
-    let chances2 = Math.floor((word.length*.15)*3);
-    chances.innerText=chances2+' oportunidades.';		    
 
 }
 
@@ -73,9 +89,12 @@ function setCharValueFortune(key){
 	if(globals['word'].length<5){
 		nregalo = 1;
 	}
-	let chances = Math.floor((globals['word'].length*.1)*3);
 	//console.log(globals['word'].length,allLetters.length,'<',nregalo+chances);
-	if((allLetters.length<=chances+nregalo)){
+
+	//si es incorrectya cero strikes
+
+	//if((allLetters.length<=chances+nregalo)){
+	if((allLetters.length<lettersWord.length)){
 		//si ya esta en algun arreglo registrado ya sea wrong wrong pos o ok, no hace nada y es valido el char y esta en la palabra
 		if (allLetters.indexOf(key)==-1 && validChars.indexOf(key)!==-1){
 			if (lettersWord.indexOf(key)!==-1){
@@ -91,6 +110,14 @@ function setCharValueFortune(key){
 				//asigna a los arreglos globales
 				globals['keys_ok'].push(key);
 			}else{
+
+				if (globals['strikes']<3){ //todiavia tiene chances
+					let st = document.getElementById('strike'+(globals['strikes']+1));
+					console.log(st);
+					console.log('strike'+globals['strikes']+1);
+					st.style.display='inline-block';
+				}
+				globals['strikes']++;
 				globals['keys_wrong'].push(key);
 			}
 			//colorea teclado
@@ -98,7 +125,7 @@ function setCharValueFortune(key){
 			
 		}
 		globals['f_chances']++;
-		if(chances-globals['f_chances']==0){
+		if(globals['strikes']==3){
 			let cbGuess = document.getElementById('optionKey');
 			//cbGuess.checked=true;
 			cbGuess.click();
@@ -119,10 +146,7 @@ function setCharValueFortune(key){
 
 	}
 
-	if(document.getElementById('chancesRecord')){
-		let quedan = chances - globals['f_chances'];
-		document.getElementById('chancesRecord').innerText=''+(quedan)+" de "+(chances)+' oportunidades';	
-	}
+
 	
 }
 
@@ -169,4 +193,15 @@ function evalAnswerFortune(){
 	}else{
 		switchView('gameView',{'res':'lost','subView':'answerView','respuesta': document.getElementById('allPhrase').innerText.toUpperCase()});
 	}			
+}
+
+
+
+
+
+
+
+function strikeImg(){
+	let el = createElementJS('div','strikeImg');
+	el.innerHTML='';
 }
